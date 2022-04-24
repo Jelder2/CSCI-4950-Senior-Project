bf1ede5b09acde837e9d47126f3f6d232d0e2328
Jake <80003118+Jelder2@users.noreply.github.com>
2022-04-23 22:51:07 -0400

    update scoreboard
    Fixed scoreboard not loading data properly

--- scoreboardinteract.php
+++ scoreboardinteract.php
@@ -1,14 +1,43 @@
 <?php
-    include 'connect.php'
+    function OpenCon(){
+    	$dbhost = "localhost";
+        $dbuser = "root";
+        $dbpass = "";
+        $db = "thegame2";
 
+	    $conn = new mysqli($dbhost, $dbuser, $dbpass, $db) or die("Connect Failure". $conn -> error);
+
+	    return $conn;
+    }
+
+    function CloseCon($conn){
+    	$conn->close();
+    }
+
+    function getNameArr(){
+        $data = array();
+        $conn = OpenCon();
+        $sql = "SELECT USER, playerScore FROM thegame ORDER BY playerScore DESC LIMIT 50;";
+        $result = $conn->query($sql);
+        while($row = $result->fetch_assoc()){
+            printf("%s (%s)\n", $row["USER"], $row["playerScore"]);
+        }
+    }
+    
     function getName(int $num, int $Offset) {
-        $select = mysql_query("SELECT 'USER' FROM 'thegame' ORDER BY 'playerScore' DESC LIMIT $num, $Offset");
-        return $select;
+        $conn = OpenCon();
+        $sql = "SELECT USER FROM thegame ORDER BY playerScore DESC LIMIT $num, $Offset;";
+        $select = $conn->query($sql);
+        $result = $select->fetch_array()[0] ?? '';
+        return $result;
     }
 
     function getScore(int $num, int $Offset) {
-        $select = mysql_query("SELECT 'playerScore' FROM 'thegame' ORDER BY 'playerScore' DESC LIMIT $num, $Offset");
-        return $select;
+        $conn = OpenCon();
+        $sql = "SELECT playerScore FROM thegame ORDER BY playerScore DESC LIMIT $num, $Offset;";
+        $select = $conn->query($sql);
+        $result = $select->fetch_array()[0] ?? '';
+        return $result;
     }
-
+    //getNameArr();
 ?>
